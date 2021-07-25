801dcf7e53ec2996e3c0381930a365b7d575c16d
tanguydamois <tdamois.com>
2021-07-25 19:13:30 +0200

    config.serve

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+config.serve_static_assets = true
+config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
